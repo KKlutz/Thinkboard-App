e6c839f3d6dfcf4091a94e2558b028435aa944d1
hizkiawdev <hizkiawdev@gmail.com>
2025-07-15 14:22:50 +0700

    4th commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // OR just '/' depending on your setup
+  base: "./", // OR just '/' depending on your setup
 });
