e484c276f5891484a649f3701e6884b72acfd689
hizkiawdev <hizkiawdev@gmail.com>
2025-07-15 14:33:09 +0700

    6th commit

--- frontend/src/lib/axios.api.js
+++ frontend/src/lib/axios.api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5050/api"
-    : "thinkboard-backend-production.up.railway.app/api";
+//const BASE_URL =
+//  import.meta.env.MODE === "development"
+//    ? "http://localhost:5050/api"
+//   : "thinkboard-backend-production.up.railway.app/api";
+const BASE_URL = "thinkboard-backend-production.up.railway.app/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
