e1daa0a0f8b91e6b1ca9f59137a2e752e7fc1ce2
hizkiawdev <hizkiawdev@gmail.com>
2025-07-15 15:03:04 +0700

    11th commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,9 @@ import { Routes, Route } from "react-router";
 import Home from "./pages/Home.jsx";
 import Create from "./pages/Create.jsx";
 import NoteDetail from "./pages/NoteDetail.jsx";
-import api from "../lib/axios.api.js";
 
 const App = () => {
   console.log("isi :::::: ....");
-  //console.log(api.get("/notes"));
   return (
     <div className="relative size-full">
       <div class="absolute size-full -z-8 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:20px_20px] [mask-image:radial-gradient(ellipse_25%_18%_at_50%_-20%,#000_10%,transparent_200%)] md:[mask-image:radial-gradient(ellipse_25%_25%_at_50%_-20%,#000_10%,transparent_200%)]" />
